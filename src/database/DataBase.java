5483371cf2e073bb04528d2d71f9c9ae9d7f3888
Khafiz <khafizturusbekuulu0@gmail.com>
2023-04-05 14:04:26 +0600

    DataBase create

--- src/database/DataBase.java
+++ src/database/DataBase.java
@@ -1,4 +1,28 @@
 package database;
 
+import classes.Hospital;
+
+import java.util.List;
+
 public class DataBase {
+    private List<Hospital> hospitals;
+
+    public DataBase(List<Hospital> hospitals) {
+        this.hospitals = hospitals;
+    }
+
+    public List<Hospital> getHospitals() {
+        return hospitals;
+    }
+
+    public void setHospitals(List<Hospital> hospitals) {
+        this.hospitals = hospitals;
+    }
+
+    @Override
+    public String toString() {
+        return "DataBase{" +
+                "hospitals=" + hospitals +
+                '}';
+    }
 }
