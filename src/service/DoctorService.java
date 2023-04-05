877cddab93900564271f77563e584f4ac2809626
Mont1k <nurtilekorozov@gmail.com>
2023-04-05 13:15:47 +0600

    Added DoctorService

---
+++ src/service/DoctorService.java
@@ -0,0 +1,15 @@
+package service;
+
+import classes.Doctor;
+
+import java.util.List;
+
+public interface DoctorService {
+    String addDoctorToHospital(Long id, Doctor Doctor);
+    Doctor findDoctorById(Long id);
+    String updateDoctor(Long id, Doctor doctor);
+    void deleteDoctorById(Long id);
+    String assignDoctorToDepartment(Long departmentId, List<Long> doctorsId);
+    List<Doctor> getAllDoctorsByHospitalId(Long id);
+    List<Doctor> getAllDoctorsByDepartmentId(Long id);
+}
